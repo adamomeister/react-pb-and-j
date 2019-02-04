0dd30e22e5dc348457e314bd02b76315124cd9f0
Tea <tealalala@protonmail.com>
2019-02-04 14:25:51 -0800

    get profit_losses data to print to developer console

--- src/components/show-backend.jsx
+++ src/components/show-backend.jsx
@@ -1,6 +1,6 @@
 // Below code to import React is essential to ensure this links with React.
 import React, { Component } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
 // This is a class component called 'FooBar.'
 class ShowBackend extends Component {
@@ -9,10 +9,10 @@ class ShowBackend extends Component {
   }
 
   componentDidMount() {
-    return fetch('http://localhost:3000/profit_losses')
+    axios.get('http://localhost:3000/api/profit_losses')
       .then(response => {
-        const allProfitLosses = response.data;
-        this.setState({ allProfitLosses });
+        const profitLosses = response.data;
+        console.log(response.data);
       })
   }
 
@@ -21,8 +21,7 @@ class ShowBackend extends Component {
       /* Insert HTML here. this is where you render the webpage. See sample: https://medium.com/javascript-in-plain-english/i-created-the-exact-same-app-in-react-and-vue-here-are-the-differences-e9a1ae8077fd
       */
       <div className="ShowBackend">
-        <h1>Hello World. This is a test {this.state.profitLosses}.</h1>
-        <h3>Call to backend { this.state.profitLosses.map(el => <li>{ el.name }</li>) }</h3>
+        <h1>Hello World. This is a test.</h1>
       </div>
     );
   }
