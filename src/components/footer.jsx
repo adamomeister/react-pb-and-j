4288c91e024671616965d79a959eda646c69a5cb
Tea <tealalala@protonmail.com>
2019-02-07 12:31:35 -0800

    combine basic components (navbar, body, footer) into main App.js

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+// Below code to import React is essential to ensure this links with React.
+import React, { Component } from 'react';
+
+// This is a class component called 'FooBar.'
+class Footer extends Component {
+  render() {
+    return (
+      <h1>Footer.</h1>
+    );
+  }
+}
+
+export default Footer;
