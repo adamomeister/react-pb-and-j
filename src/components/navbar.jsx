20ea9c4551c5f0d8106ebf81cefac37682830fe6
Tea <tealalala@protonmail.com>
2019-02-07 17:13:31 -0800

    working navbar with components for each practice page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import About from "./about";
+import ProductFeatures from "./product-features";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const NavBar = () => (
   <Router>
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">PB & J</a>
+        <a className="navbar-brand" href="/#">PB & J</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -17,11 +19,15 @@ const NavBar = () => (
             <li className="nav-item">
               <Link className="nav-link" to="/about">About</Link>
             </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/features">Features</Link>
+            </li>
           </ul>
         </div>
       </nav>
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/features" component={ProductFeatures} />
     </div>
   </Router>
 
@@ -30,42 +36,6 @@ const NavBar = () => (
 const Home = () => (
   <div>
     <h2>Home</h2>
-  </div>
-);
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.url}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
   </div>
 );
 
---
+++ src/components/product-features.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const ProductFeatures = () => (
+  <div className="container">
+    <h2>Features</h2>
+  </div>
+);
+
+export default ProductFeatures;
