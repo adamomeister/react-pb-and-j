5ec8f94e0d734ec88b4f662c68476eabe6f0922f
Tea <tealalala@protonmail.com>
2019-02-07 18:54:01 -0800

    add basic button link to OAuth sign-in on main page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <NavBar />
       </header>
       <div className="container">
-        
+        <button className="btn btn-warning"><a href="https://appcenter.intuit.com/connect/oauth2?client_id=Q0PvPlI6Vc6oEWJD4AeumjFSSLdQI4HSniUXk1weWXIGDJlawv&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&response_type=code&scope=com.intuit.quickbooks.accounting&state=randomState">OAuth Sign In</a></button>
       </div>
       <Footer />
       </div>
