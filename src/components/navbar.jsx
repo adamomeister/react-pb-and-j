0753f372d9b6558508cf3b2dc5e4f38c0c87fd65
Tea <tealalala@protonmail.com>
2019-02-07 16:33:29 -0800

    install react-router-dom via npm

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class NavBar extends Component {
   state = {
@@ -8,6 +9,7 @@ class NavBar extends Component {
 
   render() {
     return (
+      <Router>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">PB & J</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,7 +21,8 @@ class NavBar extends Component {
               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
+              // <a class="nav-link" href="product-features">Features</a>
+              <Link to="/product-features">Features</Link>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Pricing</a>
@@ -37,6 +40,9 @@ class NavBar extends Component {
           </ul>
         </div>
       </nav>
+      // <Route exact path="/" component={Home} />
+      <Route path="/product-features" component={ProductFeatures} />
+      </Router>
     );
   }
 }
---
+++ src/views/product-features.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import NavBar from './components/navbar';
+import Footer from './components/footer';
+import './index.css';
+
+class ProductFeatures extends Component {
+  render() {
+    return (
+      <div className="product-features">
+        <h1>test</h1>
+      </div>
+    );
+  }
+}
+
+export default ProductFeatures;
